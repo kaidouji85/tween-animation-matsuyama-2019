3836b6bde188e90dcc28c5a3b2db58390e650e50
竹内佑介 <kaidouji85@gmail.com>
2019-04-14 12:45:41 +0900

    tweenアニメーション入門サンプルコードを追加した

--- src/11-tween/src/sprite/game-over-label/visible.js
+++
@@ -1,8 +0,0 @@
-import {Tween} from "@tweenjs/tween.js/src/Tween";
-
-export function visible(gameOverLabel, group) {
-  const tween = new Tween(gameOverLabel, group);
-  tween.to({opacity: 1}, 3000)
-    .delay(500);
-  return tween;
-}
